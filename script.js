6c54ef6f829fb7a5fbbec72cfeb94e00e4cc35ed
Max F. Albrecht <1@178.is>
2014-01-09 18:36:26 +0100

    javascript: Besuchsdauer: rundungsfunktion

--- script.js
+++ script.js
@@ -2,36 +2,36 @@
 // Wert der Variable ist anfangs 0.
 var startzeit = 0;
 
+// mache eine funktion, die gerundet dividiert
+function dividiereGerundet(zahl, wieViel) {
+  var ergebnis = zahl / wieViel;
+  ergebnis = Math.round(ergebnis);
+  return ergebnis;
+  // oder:
+  // return Math.round(zahl / wieViel);
+}
+
 // mache eine funktion, die die startzeit setzt.
 function setzeStartzeit() {
   // wir fragen (den browser) nach der zeit,
   // indem wir die "now" funktion des eingebauten
   // "Date" objektes aufrufen:
   // ergebnis ist ein zeitstempel in milisekunden
-  var jetzt = Date.now();
-  
-  // wir teilen durch 1000 um sekunden zu erhalten
-  jetzt =  jetzt / 1000;
-  // (runde auf ganze zahlen)
-  jetzt = Math.round(jetzt);
-  
+  // ("dividiereGerundet" durch tausend)
   // speichere das ergebnis in der 'startzeit' variable
-  startzeit = jetzt;
+  startzeit = dividiereGerundet(Date.now(), 1000);
 }
 
 function errechneBesuchsdauer() {
   // wie oben, aber in einer zeile
   // da wir in einer neuen funktion sind, 
   // hat diese "jetzt" variabel NICHTS mit der oben zu tun…
-  var jetzt = Math.round(Date.now()/1000);
-  // dividiereGerundet(Date.now(), 1000)
+  var jetzt = dividiereGerundet(Date.now(), 1000);
   var ergebnis =  jetzt - startzeit;
-  // gebe das ergbnis zurück
   return ergebnis;
 }
 
 function zeigeBesuchsdauer() {
-  // errechne ergebnis, "logge" das ergbnis in der konsole
   console.log(errechneBesuchsdauer());
 }
 
